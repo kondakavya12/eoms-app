9938652b9a9716243c2ca3d35a1970e587f942b1
Kavya <kondakavya12@gmail.com>
2025-02-12 15:43:51 +0530

    fixed URL slash

--- src/api.jsx
+++ src/api.jsx
@@ -4,16 +4,16 @@ import axios from 'axios';
 const BASE_URL = 'https://eoms-backend-production.up.railway.app/';
 
 // Fetch all products from backend
-export const fetchProducts = () => axios.get(`${BASE_URL}/products/all`);
+export const fetchProducts = () => axios.get(`${BASE_URL}products/all`);
 
 // Fetch all orders from backend
-export const fetchOrders = () => axios.get(`${BASE_URL}/orders/all`);
+export const fetchOrders = () => axios.get(`${BASE_URL}orders/all`);
 
 // // Add a new order
-export const addOrder = (order) => axios.post(`${BASE_URL}/orders/add`, order);
+export const addOrder = (order) => axios.post(`${BASE_URL}orders/add`, order);
 
 // Update an existing order
-export const updateOrder = (orderId, updatedOrder) => axios.put(`${BASE_URL}/orders/update/${orderId}`, updatedOrder);
+export const updateOrder = (orderId, updatedOrder) => axios.put(`${BASE_URL}orders/update/${orderId}`, updatedOrder);
 
 // Delete an order by ID
-export const deleteOrder = (orderId) => axios.delete(`${BASE_URL}/orders/delete/${orderId}`);
+export const deleteOrder = (orderId) => axios.delete(`${BASE_URL}orders/delete/${orderId}`);
